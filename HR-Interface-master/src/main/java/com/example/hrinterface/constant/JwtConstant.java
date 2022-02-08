8fbfdcdd65b0450e4363c3da4119c427250340e9
newuser-471 <2218149303newuser@gmail.com>
2022-02-08 10:49:51 -0500

    d

--- HR-Interface-master/src/main/java/com/example/hrinterface/constant/JwtConstant.java
+++
@@ -1,8 +0,0 @@
-package com.example.hrinterface.constant;
-
-public class JwtConstant {
-    public static final String JWT_COOKIE_NAME = "JWT-LOGIN-TOKEN";
-    public static final Integer JWT_VALID_DURATION = 120;
-    public static final String JWT_REGISTERING_KEY = "RegistrationKey";
-    public static final String JWT_SIGNING_KEY = "SigningKey";
-}
