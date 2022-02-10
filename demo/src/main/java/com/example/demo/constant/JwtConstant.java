ff860945bf70830305a2aa8d2ea828fbd82bf20e
newuser-471 <2218149303newuser@gmail.com>
2022-02-10 10:24:38 -0500

    delete

--- demo/src/main/java/com/example/demo/constant/JwtConstant.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.constant;
-
-public final class JwtConstant {
-    public static final String JWT_SIGNING_KEY = "signingKey";
-    public static final String JWT_COOKIE_NAME = "JWT-TOKEN";
-}
